02bf9011a246bd2c33cca606e319ff6be7977fbd
SHEKHAR <SHEKHAR@DELL-SHEKHAR>
2020-01-04 21:37:17 +0530

    Junit and log4j2 changes

--- src/test/java/com/url/app/FmsApplicationTests.java
+++ src/test/java/com/url/app/FmsApplicationTests.java
@@ -3,15 +3,12 @@ package com.url.app;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.jasypt.encryption.StringEncryptor;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class FmsApplicationTests {
 	private static final Logger logger = LoggerFactory.getLogger(FmsApplicationTests.class);
